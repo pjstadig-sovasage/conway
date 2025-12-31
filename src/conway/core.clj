83dbd075647329e75214355a4c1645d83851c81c
Paul Stadig <paul.stadig@sovasage.com>
2025-12-31 12:07:53 -0500

    Implement count-neighbors and add new test

--- src/conway/core.clj
+++ src/conway/core.clj
@@ -1,9 +1,15 @@
-(ns conway.core 
+(ns conway.core
   (:require
    [clojure.string :as str]))
 
 (defn print-grid [grid]
    (println (str/join "\n" (map #(str/join %) grid))))
-    
-(defn count-neighbors [grid row col])
-  
+
+(def neighbors [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1]])
+
+(defn get-neighbors
+  [grid row col]
+  (for [[x y] neighbors] (get-in grid [(+ row x) (+ col y)])))
+
+(defn count-neighbors [grid row col]
+  (transduce (map {" " 0 "O" 1}) + 0 (get-neighbors grid row col)))
--- test/conway/core_test.clj
+++ test/conway/core_test.clj
@@ -4,10 +4,37 @@
     [conway.core :as core]))
 
 (deftest t-print-grid-prints-a-grid
-  (is (= "   \n   \n   \n" (with-out-str (core/print-grid [[" " " " " "] [" " " " " "] [" " " " " "]])))))
+  (is (= "   \n   \n   \n" (with-out-str (core/print-grid [[" " " " " "]
+                                                           [" " " " " "]
+                                                           [" " " " " "]]))))
+  (is (= "O  \n  O\n   \n" (with-out-str (core/print-grid [["O" " " " "]
+                                                           [" " " " "O"]
+                                                           [" " " " " "]])))))
 
-(deftest t-count-neighbors 
-  (is (= 0 (core/count-neighbors [[" " " " " "] [" " "O" " "] [" " " " " "]] 1 1)))
-  (is (= 1 (core/count-neighbors [[" " " " " "] ["0" "O" " "] [" " " " " "]] 1 1)))
-  (is (= 3 (core/count-neighbors [["O" " " "O"] [" " "O" " "] ["O" " " " "]] 1 1)))
-  (is (= 4 (core/count-neighbors [[" " "O" " "] [" " " " "O"] ["O" "O" " "]] 1 1))))
+(deftest t-count-neighbors
+  (is (= 0 (core/count-neighbors [[" " " " " "]
+                                  [" " "O" " "]
+                                  [" " " " " "]] 1 1)))
+  (is (= 1 (core/count-neighbors [[" " " " " "]
+                                  ["O" "O" " "]
+                                  [" " " " " "]] 1 1)))
+  (is (= 3 (core/count-neighbors [["O" " " "O"]
+                                  [" " "O" " "]
+                                  ["O" " " " "]] 1 1)))
+  (is (= 4 (core/count-neighbors [[" " "O" " "]
+                                  [" " " " "O"]
+                                  ["O" "O" " "]] 1 1))))
+
+(deftest t-count-neighbors-at-edge
+  (is (= 0 (core/count-neighbors [["O" " " " "]
+                                  [" " " " " "]
+                                  [" " " " " "]] 0 0)))
+  (is (= 1 (core/count-neighbors [["O" " " " "]
+                                  ["O" " " " "]
+                                  [" " " " " "]] 0 0)))
+  (is (= 3 (core/count-neighbors [["O" " " "O"]
+                                  ["O" " " " "]
+                                  ["O" " " " "]] 0 0)))
+  (is (= 4 (core/count-neighbors [[" " "O" " "]
+                                  [" " " " "O"]
+                                  ["O" "O" " "]] 0 0))))
