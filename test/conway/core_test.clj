e12ddea78eab6424bf98816907a128f7fba70d13
Paul Stadig <paul.stadig@sovasage.com>
2025-12-31 11:31:24 -0500

    Reset

--- test/conway/core_test.clj
+++ test/conway/core_test.clj
@@ -2,23 +2,3 @@
   (:require
     [clojure.test :refer [deftest is]]
     [conway.core :as core]))
-
-(deftest t-step
-  (let [grid [[0 0 0] [0 0 0] [0 0 0]]]
-    (is (= grid (core/step grid)))))
-
-(deftest t-step-live-cell-dies
-  (let [grid [[0 0 0] [0 1 0] [0 0 0]]]
-    (is (= [[0 0 0] [0 0 0] [0 0 0]] (core/step grid)))))
-
-(deftest t-step-dead-cell-lives
-  (let [grid [[0 0 1] [0 0 0] [1 1 0]]]
-    (is (= [[0 0 1] [0 1 0] [1 1 0]] (core/step grid)))))
-
-(deftest t-step-update-multiple-cells
-  (let [grid [[0 0 1]
-              [0 0 0]
-              [1 1 0]]]
-    (is (= [[0 0 0]
-            [0 1 0]
-            [0 1 0]] (core/step grid)))))
