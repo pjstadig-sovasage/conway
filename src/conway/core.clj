40b480f1529104bb97f146af7e7423c9e9ea9b42
Chase Lambert <chaselambert@gmail.com>
2025-12-31 11:52:45 -0500

    count-neighbors

--- src/conway/core.clj
+++ src/conway/core.clj
@@ -1,5 +1,9 @@
-(ns conway.core)
+(ns conway.core 
+  (:require
+   [clojure.string :as str]))
 
-(defn print-grid
-  [grid]
-  )
+(defn print-grid [grid]
+   (println (str/join "\n" (map #(str/join %) grid))))
+    
+(defn count-neighbors [grid row col])
+  
--- test/conway/core_test.clj
+++ test/conway/core_test.clj
@@ -4,4 +4,10 @@
     [conway.core :as core]))
 
 (deftest t-print-grid-prints-a-grid
-  (is (= "   \n   \n   \n" (core/print-grid [[0 0 0] [0 0 0] [0 0 0]]))))
+  (is (= "   \n   \n   \n" (with-out-str (core/print-grid [[" " " " " "] [" " " " " "] [" " " " " "]])))))
+
+(deftest t-count-neighbors 
+  (is (= 0 (core/count-neighbors [[" " " " " "] [" " "O" " "] [" " " " " "]] 1 1)))
+  (is (= 1 (core/count-neighbors [[" " " " " "] ["0" "O" " "] [" " " " " "]] 1 1)))
+  (is (= 3 (core/count-neighbors [["O" " " "O"] [" " "O" " "] ["O" " " " "]] 1 1)))
+  (is (= 4 (core/count-neighbors [[" " "O" " "] [" " " " "O"] ["O" "O" " "]] 1 1))))
